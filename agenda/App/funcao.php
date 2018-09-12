599970d10f2b174f73e301c6c3564d4982a7afe0
ProgramadorWebTI <programadorwebti@gmail.com>
2018-09-12 10:46:56 -0300

    Criando a função de selecionar todos os contatos da agenda

--- agenda/App/funcao.php
+++ agenda/App/funcao.php
@@ -1,2 +1,15 @@
 <?php
 
+// CRIAR A FUNÇÃO DE SELECIONA TODA AGENDA
+
+function selecionaAgenda() {
+    $sql  = "SELECT * FROM contato";
+    $stmt = pdo()->prepare($sql);
+    $stmt->execute();
+    if($stmt->rowCount() > 0) {
+        return $stmt->fetch(\PDO::FETCH_OBJ);
+    } else {
+        return 0;
+    }
+
+}
