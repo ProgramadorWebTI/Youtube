202786de1ab400081452a7276b757beb97ba8286
ProgramadorWebTI <programadorwebti@gmail.com>
2018-09-12 10:51:21 -0300

    Criando a função de inserir novo contato à agenda

--- agenda/App/funcao.php
+++ agenda/App/funcao.php
@@ -13,3 +13,16 @@ function selecionaAgenda() {
     }
 
 }
+
+//  CRIAR FUNÇÃO PARA INSERIR UM NOVO CONTATO NA AGENDA
+
+function inserirNovoContato() {
+    $sql = "INSERT INTO contato (con_nome,con_fone1,con_fone2,con_cel,con_email) VALUES (?,?,?,?,?)";
+    $stmt = pdo()->prepare($sql);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
