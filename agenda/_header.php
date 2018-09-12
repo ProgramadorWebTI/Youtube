35afea323ba67c903aa66dbc823def5581ac25d9
ProgramadorWebTI <programadorwebti@gmail.com>
2018-09-12 10:40:50 -0300

    projeto base

---
+++ agenda/_header.php
@@ -0,0 +1,16 @@
+<?php 
+
+// INCLUIR O AUTOLOAD PARA O CARREGAMENTO DAS FUNÇÕES AUTOMÁTICAS;
+require_once "./vendor/autoload.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
