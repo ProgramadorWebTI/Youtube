a92f851744f962ea94bb58fae7e57990e55079e5
ProgramadorWebTI <programadorwebti@gmail.com>
2018-09-12 10:57:59 -0300

    Criando a função de atualizar o contato

--- agenda/App/funcao.php
+++ agenda/App/funcao.php
@@ -26,3 +26,22 @@ function inserirNovoContato() {
         return 0;
     }
 }
+
+// CRIAR A FUNÇÃO DE ATUALIZAR UM REGISTRO NA AGENDA
+
+function editarContato($con_nome, $con_fone1, $con_fone2, $con_cel, $con_email, $con_codigo) {
+    $sql = "UPDATE contato SET con_nome=?,con_fone1=?,con_fone2=?,con_cel=?,con_email=? WHERE con_codigo =?";
+    $stmt = pdo()->prepare($sql);
+    $stmt->bindValue(1, $con_nome);
+    $stmt->bindValue(2, $con_fone1);
+    $stmt->bindValue(3, $con_fone2);
+    $stmt->bindValue(4, $con_cel);
+    $stmt->bindValue(5, $con_email);
+    $stmt->bindValue(6, $con_codigo);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
